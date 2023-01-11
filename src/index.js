a205cd56c0b0e3bd910edc9281b0d781bbec8860
Willem <70796630+uhWillem@users.noreply.github.com>
2023-01-11 17:03:01 +0100

    Fixed edit button

--- src/index.js
+++ src/index.js
@@ -22,47 +22,43 @@ function loadStudents(endpoint) {
 function createStudentElement(student) {
    return `
      <tr class="focus:outline-none h-14 bg-white border border-gray-100 text-gray-500 text-md font-medium hover:text-gray-800 hover:bg-gray-50" onclick="viewStudentButton(this)">
-       <td>
-         <div class="flex items-center pl-5">
-           <p>${student.first_name} ${student.last_name}</p>
-         </div>
-       </td>
-       <td class="pl-24">
-         <div class="flex items-center">
-           <p class="ml-2">${student.email}</p>
-         </div>
-       </td>
-       <td class="pl-5">
-         <div class="flex items-center">
-           <p class="ml-2">${student.course}</p>
-         </div>
-       </td>
-       <td class="pl-5">
-         <div class="flex items-center">
-           <p class="ml-2">${student.sex}</p>
-         </div>
-       </td>
-       <td class="pl-5">
-         <div class="flex items-center">
-           <p class="ml-2">${student.birthyear}</p>
-         </div>
-       </td>
-       <td>
-         <div class="pr-4">
+    <td>
+       <div class="flex items-center pl-5">
+          <p>${student.first_name} ${student.last_name}</p>
+       </div>
+    </td>
+    <td class="pl-24">
+       <div class="flex items-center">
+          <p class="ml-2">${student.email}</p>
+       </div>
+    </td>
+    <td class="pl-5">
+       <div class="flex items-center">
+          <p class="ml-2">${student.course}</p>
+       </div>
+    </td>
+    <td class="pl-5">
+       <div class="flex items-center">
+          <p class="ml-2">${student.sex}</p>
+       </div>
+    </td>
+    <td class="pl-5">
+       <div class="flex items-center">
+          <p class="ml-2">${student.birthyear}</p>
+       </div>
+    </td>
+    <td>
+       <div class="pr-4">
            <button onclick="editStudentButton(this); event.stopPropagation()" data-student='${JSON.stringify(student)}' class="mt-4 sm:mt-0 inline-flex items-start justify-start py-3 px-8 rounded-lg float-right">
              <p class="customcolorblue">Edit</p>
-           </button>
-         </div>
-       </td>
-     </tr>
-   `;
- }
- 
-
-
-function editStudent() {
-   console.log("testedit")
+          </button>
+       </div>
+    </td>
+ </tr>
+    `;
 }
+
+
 
 function editStudentButton(data) {
 
@@ -82,6 +78,7 @@ function editStudentButton(data) {
    document.getElementById("edit_phone_number").value = student.phone_number;
    document.getElementById("edit_city").value = student.city;
    document.getElementById("edit_note").value = student.note;
+   document.getElementById("edit_student_id").value = student.student_id;
 }
 
 function viewStudentButton(data) {
@@ -90,6 +87,53 @@ function viewStudentButton(data) {
    const addStudentModal = document.getElementById("viewStudent");
    addStudentModal.classList.remove("hidden");
    addStudentModal.classList.add("flex");
+
+}
+
+function editStudent() {
+   // Get the values from the form
+   let first_name = document.getElementById("edit_first_name").value;
+   let last_name = document.getElementById("edit_last_name").value;
+   let course = document.getElementById("edit_course").value;
+   let birthyear = document.getElementById("edit_birthyear").value;
+   let sex = document.getElementById("edit_sex").value;
+   let email = document.getElementById("edit_email").value;
+   let phone_number = document.getElementById("edit_phone_number").value;
+   let city = document.getElementById("edit_city").value;
+   let note = document.getElementById("edit_note").value;
+   let id = document.getElementById("edit_student_id").value;
+
+   // Create the student object
+   const student = {
+      first_name: first_name,
+      last_name: last_name,
+      course: course,
+      birthyear: birthyear,
+      sex: sex,
+      email: email,
+      phone_number: phone_number,
+      city: city,
+      note: note,
+   };
+
+   // Send the student object to the server
+   fetch(`http://104.131.62.197/api/students/id/${id}`, {
+      method: "PUT",
+      headers: {
+         "Content-Type": "application/json",
+      },
+      body: JSON.stringify(student),
+   })
+      .then((response) => response.json())
+      .then((data) => {
+         console.log("Success:", data);
+         loadStudents("api/students");
+      })
+      .catch((error) => {
+         console.error("Error:", error);
+      });
+
+
 
 }
 
